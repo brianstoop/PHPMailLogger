3e83b088b78b24caa8df6500d81dd3afd8e15217
Brian Stoop <brian.stoop@moveagency.com>
2025-01-28 15:05:59 +0100

    Logger: Extend PHPMailer with hooks for logging
    Issue: HKGO-2249

---
+++ src/Lunr/Ticks/PHPMailer/PHPMailer.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * This file contains the PHPMailer class.
+ *
+ * SPDX-FileCopyrightText: Copyright 2025 Move Agency Group B.V., Zwolle, The Netherlands
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace Lunr\Ticks\PHPMailer;
+
+use Lunr\Ticks\EventLogging\EventLoggerInterface;
+use PHPMailer\PHPMailer\PHPMailer as BaseMailer;
+
+/**
+ * PHPMailer class
+ */
+class PHPMailer extends BaseMailer
+{
+
+    /**
+     * Shared instance of the mail logger
+     * @var EventLoggerInterface
+     */
+    protected readonly EventLoggerInterface $logger;
+
+    /**
+     * Profiling level
+     * @var int
+     */
+    protected int $level;
+
+    /**
+     * Start time of the mail sending
+     * @var float
+     */
+    protected float $startTime;
+
+    /**
+     * Constructor.
+     *
+     * @param EventLoggerInterface $logger     PHP mail logger
+     * @param int                  $level      Profiling level
+     * @param bool|null            $exceptions Should we throw external exceptions?
+     */
+    public function __construct(EventLoggerInterface $logger, int $level, ?bool $exceptions = NULL)
+    {
+        $this->logger = $logger;
+        $this->level  = $level;
+
+        parent::__construct($exceptions);
+
+        /**
+         * The type of action_function is defined as string but it should be callable, so we ignore the phpstan warning
+         * @phpstan-ignore assign.propertyType
+         */
+        $this->action_function = [ $this, 'afterSending' ];
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        unset($this->startTime);
+        unset($this->level);
+
+        parent::__destruct();
+    }
+
+    /**
+     * Create a message and send it.
+     * Uses the sending method specified by $Mailer.
+     *
+     * @return bool false on error - See the ErrorInfo property for details of the error
+     */
+    public function send(): bool
+    {
+        $this->startTime = microtime(TRUE);
+
+        return parent::send();
+    }
+
+    /**
+     * Callback after each mail send
+     *
+     * @param bool                                          $isSent  Result of the send action
+     * @param array{0: string,1?: string}                   $to      Email addresses of the recipients
+     * @param array<array{0: string,1?: string}>            $cc      Cc email addresses
+     * @param array<array{0: string,1?: string}>            $bcc     Bcc email addresses
+     * @param string                                        $subject The subject
+     * @param string                                        $body    The email body
+     * @param string                                        $from    Email address of sender
+     * @param array{smtp_transaction_id?: bool|string|null} $extra   Extra information of possible use
+     *
+     * @return void
+     */
+    protected function afterSending(bool $isSent, array $to, array $cc, array $bcc, string $subject, string $body, string $from, array $extra): void
+    {
+        $tags = [
+            'type'   => $this->Mailer,
+            'status' => $isSent ? 'true' : 'false',
+        ];
+
+        $fields = [
+            'url'      => $this->Host,
+            'duration' => microtime(TRUE) - $this->startTime,
+            'ip'       => gethostbyname($this->Host),
+        ];
+
+        $options = [
+            'subject' => $subject,
+            'body'    => $body,
+            'from'    => $from,
+        ];
+
+        $options = json_encode($options + $extra);
+
+        // If the profiling level is not INFO we want to log extra info
+        if ($this->level !== 0)
+        {
+            $fields['request_headers'] = $this->prepareLogData($this->MIMEHeader);
+            $fields['data']            = $this->prepareLogData($this->MIMEBody);
+            $fields['options']         = $this->prepareLogData(is_bool($options) ? '' : $options);
+        }
+
+        $event = $this->logger->newEvent('mail');
+
+        $event->addTags($tags);
+        $event->addFields($fields);
+        $event->recordTimestamp();
+        $event->record();
+    }
+
+    /**
+     * Prepare data according to loglevel.
+     *
+     * @param string $data Data to prepare for logging.
+     *
+     * @return string Prepare data to log.
+     */
+    private function prepareLogData(string $data): string
+    {
+        // If the profiling level is DEBUG we want to log part of the info
+        if ($this->level === 1 && strlen($data) > 512)
+        {
+            return substr($data, 0, 512) . '...';
+        }
+
+        return $data;
+    }
+
+}
+
+?>
